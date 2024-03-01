8c57d76d46dbd6bfadfb7facfbbab0f5109adee4
Gülşah Düzgün <65841330+GulsahDuzgun@users.noreply.github.com>
2024-03-01 15:28:11 +0300

    CHALLENGE #5

--- CHALLENGE #5/script.js
+++ CHALLENGE #5/script.js
@@ -9,9 +9,9 @@ const scoreDolphins2 = calcAverage(85, 54, 41);
 const scoreKoalas2 = calcAverage(23, 34, 27);
 
 function checkWinner(avgDolphins, avgKoalas) {
-  if (avgDolphins > 2 * avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
     console.log(`Dolphins win ${avgDolphins}`);
-  } else if (avgKoalas > 2 * avgDolphins) {
+  } else if (avgKoalas >= 2 * avgDolphins) {
     console.log(`Koalas win ${avgKoalas}`);
   } else {
     console.log("No team wins...");
